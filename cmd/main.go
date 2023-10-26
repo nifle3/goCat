ec19ce233e3ffe2fc19588d2c3602b333ce0c168
nifle3 <kuper116rus.2005@gmail.com>
2023-10-27 00:07:43 +0300

    add basic file enter

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Hi, this program is designed for read text files, " +
+			"please enter a path of file, you want to read\n")
+
+		return
+	}
+
+	filePath := os.Args[1]
+	file, err := os.Open(filePath)
+	if os.IsNotExist(err) {
+		fmt.Printf("Please enter a correct path, your path is %s\n", filePath)
+
+		return
+	}
+	defer file.Close()
 
 }
